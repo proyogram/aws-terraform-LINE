699b5358a18c4d36a23901bbf3401665818c5bd2
proyogram <proyogram@gmail.com>
2023-07-16 04:14:44 +0900

    terraform fmtによる差分

--- modules/stop_running_ec2/variables.tf
+++ modules/stop_running_ec2/variables.tf
@@ -3,7 +3,7 @@ variable "prefix" {
   description = "全てのリソースの文字前に付与される値"
 }
 
-variable "secret_id_key"{
+variable "secret_id_key" {
   type        = string
   description = "Secrets ManagerからLINEのアクセストークンを取ってくるためのシークレットキー"
 }
