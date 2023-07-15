145c6f8fcd46e0743699ccd9354b9f768e7e59ba
proyogram <proyogram@gmail.com>
2023-07-16 01:23:42 +0900

    適切なprefixに変更

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@
 
 module "stop_running_ec2" {
   source = "./modules/stop_running_ec2"
-  prefix = "tf-01-notice-running-ec2-with-line"
+  prefix = "tf-line-stop-running-ec2"
   secret_id_key = "LINE_ACCESS_TOKEN_STOP_EC2"
 }
