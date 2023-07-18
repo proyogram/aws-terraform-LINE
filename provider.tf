975c20e5b6d20cbdb9ba1d6857fe19435f492268
proyogram <proyogram@gmail.com>
2023-07-18 21:16:04 +0900

    backendをs3に変更

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+terraform {
+  required_version = ">= 0.11.0"
+  backend "s3" {
+    bucket         = "tf-state-backend-wakatay"
+    region         = "ap-northeast-1"
+    key            = "LINE/terraform.tfstate"
+    encrypt        = true
+  }
+}
