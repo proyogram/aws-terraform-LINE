8139a458195a38888d172db4aac687ef3d66f63f
proyogram <proyogram@gmail.com>
2023-07-22 12:16:53 +0900

    fmt

--- provider.tf
+++ provider.tf
@@ -5,9 +5,9 @@ provider "aws" {
 terraform {
   required_version = ">= 1.4.0"
   backend "s3" {
-    bucket         = "tf-state-backend-wakatay"
-    region         = "ap-northeast-1"
-    key            = "LINE/terraform.tfstate"
-    encrypt        = true
+    bucket  = "tf-state-backend-wakatay"
+    region  = "ap-northeast-1"
+    key     = "LINE/terraform.tfstate"
+    encrypt = true
   }
 }
