58dda16b51bf047ca3d9bca95f7709cca217d0f9
proyogram <proyogram@gmail.com>
2023-07-22 12:25:09 +0900

    backend用バケットの変更

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   required_version = ">= 1.4.0"
   backend "s3" {
-    bucket  = "tf-state-backend-wakatay"
+    bucket  = "tf-backend-proyogram"
     region  = "ap-northeast-1"
     key     = "LINE/terraform.tfstate"
     encrypt = true
